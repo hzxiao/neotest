850930cad6c155cf70a0a1ae6a8c99a249c94a6d
Xiaozh <838223935@qq.com>
2019-07-25 21:58:39 +0800

    :white_check_mark: test split expr

--- source_test.go
+++ source_test.go
@@ -1,7 +1,9 @@
 package neotest
 
 import (
+	"bufio"
 	"github.com/hzxiao/goutil/assert"
+	"strings"
 	"testing"
 )
 
@@ -12,4 +14,24 @@ func TestValidID(t *testing.T) {
 	assert.True(t, ValidID("a_b_1"))
 	assert.False(t, ValidID("@ab"))
 	assert.False(t, ValidID("1ab"))
+}
+
+func TestSplitExpr(t *testing.T) {
+	assert.Equal(t, []string(nil), splitRawExpr(""))
+	assert.Equal(t, []string{"x"}, splitRawExpr("x"))
+	assert.Equal(t, []string{"x"}, splitRawExpr(" x "))
+	assert.Equal(t, []string{"echo", "1", "2"}, splitRawExpr("echo 1 2"))
+	assert.Equal(t, []string{"let", "@host", "`env HOST`"}, splitRawExpr("let @host `env HOST`"))
+}
+
+func splitRawExpr(text string) []string {
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	scanner.Split(splitExpr)
+
+	var rawExprs []string
+	for scanner.Scan() {
+		rawExprs = append(rawExprs, scanner.Text())
+	}
+
+	return rawExprs
 }
